db4fb2333bc29f5013964e5921978a07cde5759d
13rentgen <13rentgen@gmail.com>
2016-08-31 02:36:17 +0300

    Npm package

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ *  __   __   __   __   __        ___  __        ___  __
+ * |__) |__) /  \ |__) /  ` |__| |__  /  ` |__/ |__  |__)
+ * |    |  \ \__/ |    \__, |  | |___ \__, |  \ |___ |  \
+ *
+ * Copyright(c) 2016 Aleksandr Zaytsev
+ * MIT Licensed
+ */
+'use strict';
+
+module.exports = {};
